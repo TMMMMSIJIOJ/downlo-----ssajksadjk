ac31a6a2d3feea429db0a82d220f4ec72a966e58
AwongjiamingA <85433535+AwongjiamingA@users.noreply.github.com>
2021-06-22 19:50:10 +0800

    Create m.php

---
+++ m.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=0.5, maximum-scale=2.0, user-scalable=yes"/>
+<meta http-equiv="Cache-Control" content="no-siteapp" />
+<title>SKY 4K | 天空 4K</title>
+<meta name="Keywords" content="SKY 4K | 天空 4K" />
+<meta name="Description" content="SKY 4K | 天空 4K" />
+<br>
+<!---------------------------------->
+ <script>
+function is_mobile() {  
+var regex_match = /(nokia|iphone|android|motorola|^mot-|softbank|foma|docomo|kddi|up.browser|up.link|htc|dopod|blazer|netfront|helio|hosin|huawei|novarra|CoolPad|webos|techfaith|palmsource|blackberry|alcatel|amoi|ktouch|nexian|samsung|^sam-|s[cg]h|^lge|ericsson|philips|sagem|wellcom|bunjalloo|maui|symbian|smartphone|midp|wap|phone|windows ce|iemobile|^spice|^bird|^zte-|longcos|pantech|gionee|^sie-|portalmmm|jigs browser|hiptop|^benq|haier|^lct|operas*mobi|opera*mini|320x320|240x320|176x220)/i;  
+var u = navigator.userAgent;  
+if (null == u) {return true;}  
+var result = regex_match.exec(u);  
+if (null == result) {return false}   
+else {return true}  
+}  
+if (is_mobile()) {document.location.href= 'index.html';} 
+ </script>
+<!---------------------------------->
+<style>
+body{padding:0;background:#000000;width:100%; text-align: center;}
+.main,.btn{margin:30px auto;}
+.logo{width:300px; margin:0 auto}
+p{ text-align:center; color:#9F9F9F; max-width:70%; margin:10px auto;line-height:1.4;font-family: "微软雅黑"}
+h1{ text-align:center;color:#D3D3D3}
+.site{ max-width: 80%; margin: 0 auto;}
+.btn{ display:inline-block;width:80px;height:40px;  line-height:40px;color:#e62b2b; text-decoration:none; background:transparent; border: 1px solid #CCCCCC; border-radius: 5px;text-align:center;font-weight:bold;font-size:22px; }
+.btn:hover{color: #e62b2b; background-color:transparent;}
+a{color: #17ff4d; text-decoration: none;}
+.ads{ width:100%; height:300px}
+.zhu{ margin: 0 auto; align-content: center;}
+
+.beiyong a{ margin: 10px 20px;display:inline-block}
+.beiyong a:hover{ color:transparent;}
+</style>
+</head>
+
+<body class="main">
+<div>
+<h1><span style="color:red">SKY 4K | 天空 4K</span></h1>
+<p class="statement">
+TELEGRAM GROUP<a style="font-weight: bold; color: red;">&nbsp;</a>@sky4ktv @sky_4k_app<br>
+</p>
+<!------背景图-
+---->
+<script type="text/javascript">
+var bodyBgs = [];
+bodyBgs[0] = "https://i.ibb.co/F7ZxmjT/download-2.jpg";
+bodyBgs[1] = "https://i.ibb.co/F7ZxmjT/download-2.jpg";
+bodyBgs[2] = "https://i.ibb.co/F7ZxmjT/download-2.jpg";
+
+
+setInterval(Getbg, 5000);
+function Getbg(){
+  var randomBgIndex = Math.round( Math.random() * 11 );
+  //输出随机的背景图
+  document.body.style.background="#141414 url("+ bodyBgs[randomBgIndex] +") no-repeat ";
+}
+</script>
+
+
+<div class="site">
+	<div class="zhu">
+		<div class="btn"><a rel="nofollow" href="https://bit.ly/3vNzoW3" target="_blank">OLD</a></div>&nbsp;&nbsp;&nbsp;
+		<div class="btn"><a rel="nofollow" href="https://bit.ly/3qf9OIi" target="_blank">NEW</a></div>&nbsp;&nbsp;&nbsp;
+
+  </div>
+
+   <!---div class="zhu">
+	</div--->
+
+</div>
+
+</body>
+
+
+</html>
